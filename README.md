f9872eaa8f72a01dac658f40660fd291c86c5767
patrickneugebauer <patrickneugebauer@users.noreply.github.com>
2018-08-26 18:36:51 -0400

    Add f#.

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 #### Birthday Paradox - Monte Carlo simulations
 
 * sample-size: 23
-* probability: 50.75
+* probability: 50.72
 
 language | iterations/sec
 |--|--|
-c|3,996,802
-crystal|3,021,148
-csharp|2,346,316
-rust|2,146,383
-dart|1,626,016
-java|1,349,527
-go|1,338,688
-javascript|1,283,697
+c|3,815,337
+crystal|3,018,412
+csharp|2,360,717
+rust|2,146,844
+dart|1,615,508
+java|1,331,557
+go|1,328,021
+javascript|1,298,701
 ruby|239,234
-julia|125,786
-haskell|96,153
-python|42,087
-python3|26,560
-R|9,199
-bash|3,979
-powershell|415
+julia|127,551
+haskell|95,877
+fsharp|67,249
+python|44,662
+python3|25,380
+R|9,310
+bash|3,995
+powershell|422
--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -2,7 +2,37 @@
 
 open System
 
+let random = Random()
+
+let rec duplicates n xs =
+  match n with
+  | 0 -> false
+  | _ -> let num = random.Next(365)
+         if List.contains num xs then true else duplicates (n-1) (num::xs)
+
+let inline println s i =
+  s + string(i) |> printfn "%s"
+
+let simulate =
+  let start = DateTime.Now
+  let iterations = 100000
+  let sampleSize = 23
+  let mutable count = 0
+  for i in 1..iterations do
+    if duplicates sampleSize [] then count <- count + 1
+  println "iterations: " iterations
+  println "sample-size: " sampleSize
+  float(count) / float(iterations) * 100.0
+    |> fun x -> Math.Round(x, 2)
+    |> println "percent: "
+  let fin = DateTime.Now
+  fin - start
+    |> fun x -> x.TotalSeconds
+    |> fun x -> Math.Round(x, 3)
+    |> println "seconds: "
+  ()
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    simulate
     0 // return an integer exit code
