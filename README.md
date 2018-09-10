0d2d1bc7c4d5c8714c39eed1c95fd5face5a09f9
patrickneugebauer <patrickneugebauer@users.noreply.github.com>
2018-09-09 20:57:54 -0400

    Add kotlin.

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 #### Birthday Paradox - Monte Carlo simulations
 
 * sample-size: 23
-* probability: 50.72
+* probability: 50.78
 
 language | iterations/sec
 |--|--|
-c|3,815,337
-crystal|3,018,412
-csharp|2,360,717
-rust|2,146,844
-dart|1,615,508
-java|1,331,557
-go|1,328,021
-javascript|1,298,701
-ruby|239,234
-julia|127,551
-haskell|95,877
-fsharp|67,249
-python|44,662
-python3|25,380
-R|9,310
-bash|3,995
-powershell|422
+c|3,892,565
+crystal|2,679,528
+csharp|2,372,479
+rust|2,145,462
+kotlin|2,008,032
+dart|1,626,016
+go|1,342,281
+java|1,342,281
+javascript|1,290,322
+ruby|241,545
+julia|127,388
+haskell|95,785
+fsharp|67,204
+python|44,603
+python3|25,906
+R|9,372
+bash|3,909
+powershell|424
---
+++ kotlin/loops.kt
@@ -0,0 +1,33 @@
+import java.util.Random
+
+fun main(args: Array<String>) {
+  simulate()
+}
+
+fun simulate() {
+  val start = System.currentTimeMillis();
+  val iterations = 1000000
+  val sample_size = 23
+  val random = Random()
+
+  var count = 0
+  for (i in 1..iterations) {
+    val data = IntArray(365);
+    for (n in 1..sample_size) {
+      val number = random.nextInt(365)
+      if (data[number] == 1) {
+        count++
+        break
+      } else {
+        data[number] = 1
+      }
+    }
+  }
+  println("iterations: ${iterations}")
+  println("sample-size: ${sample_size}")
+  val percent = count * 100.0 / iterations
+  println("percent: %.2f".format(percent))
+  val end = System.currentTimeMillis();
+  val diff = (end - start) / 1000.0
+  println("seconds: ${diff}")
+}
