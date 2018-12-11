57039bb81acf9acf636ea0aa1b09f1029b1aa8ea
patrickneugebauer <patrickneugebauer@users.noreply.github.com>
2018-12-10 23:25:57 -0500

    Use IntSet in haskell.

--- README.md
+++ README.md
@@ -1,33 +1,33 @@
 #### Birthday Paradox - Monte Carlo simulations
 
 * sample-size: 23
-* probability: 50.98
+* probability: 50.84
 
 | | language | iterations/sec |
 |--|--|--|
-1|c|4,016,064
-2|crystal|2,894,356
-3|csharp|2,352,941
-4|vb.net|2,202,643
-5|kotlin|2,169,197
-6|rust|2,143,622
-7|java|2,087,682
-8|dart|1,612,903
-9|go|1,345,895
-10|javascript|1,278,772
-11|php|469,924
-12|hack|301,932
-13|ruby|241,254
-14|python3|186,219
-15|python|170,357
-16|perl|152,671
-17|julia|125,786
-18|swift|100,401
-19|haskell|96,339
-20|fsharp|65,445
-21|R|9,267
-22|perl6|4,226
-23|bash|4,000
-24|powershell|409
+1|c|4,022,526
+2|crystal|2,777,777
+3|csharp|2,347,417
+4|vb.net|2,212,389
+5|kotlin|2,200,220
+6|java|2,195,389
+7|rust|2,129,925
+8|dart|1,693,480
+9|go|1,333,333
+10|javascript|1,275,510
+11|php|466,853
+12|hack|286,041
+13|ruby|241,545
+14|python3|187,265
+15|perl|160,256
+16|python|153,846
+17|julia|125,628
+18|haskell|102,986
+19|swift|96,993
+20|fsharp|64,516
+21|R|9,033
+22|perl6|4,128
+23|bash|3,843
+24|powershell|411
 
 thanks [Anthony Robinson](https://github.com/anthonycrobinson) for the tip about randint and random speed in python
--- haskell/loops.hs
+++ haskell/loops.hs
@@ -1,49 +1,32 @@
 import System.Random
 import Data.Time.Clock.POSIX
+import qualified Data.IntSet as IntSet
 
 getTimeMillis :: (Integral a) => IO a
 getTimeMillis = round <$> (*1000) <$> getPOSIXTime
 
-duplicates :: (Eq a) => [a] -> Bool
-duplicates [] = False
-duplicates (x:xs) = x `elem` xs || duplicates xs
-
 roundTo :: (Integral a, RealFrac b, Fractional c) => a -> b -> c
 roundTo n = (/10^n) . fromIntegral . round . (*10^n)
-
-randomRs' :: (RandomGen g, Random a, Eq a, Num n, Eq n) => (a, a) -> n -> g -> ([a], g)
-randomRs' _ 0 gen = ([], gen)
-randomRs' (l, u) n gen =
-  let (r, midGen) = randomR (l, u) gen
-      (xs, finalGen) = randomRs' (l, u) (n-1) midGen
-  in (r:xs, finalGen)
-
-simulate :: (Num n, Eq n) => n -> StdGen -> [Bool]
-simulate 0 gen = []
-simulate n gen =
-  let (rs, midGen) = randomRs' (0, 364) 23 gen :: ([Int], StdGen)
-  in duplicates rs:simulate (n-1) midGen
 
 simulate' :: Int -> StdGen -> Int
 simulate' 0 gen = 0
 simulate' n gen =
   let range = (0, 364) :: (Int, Int)
       sampleSize = 23 :: Int
-      sample = [] :: [Int]
+      sample = IntSet.empty :: IntSet.IntSet
       (x, newGen) = runIteration range sampleSize sample gen
   in x + simulate' (n - 1) newGen
 
-runIteration :: (Int, Int) -> Int -> [Int] -> StdGen -> (Int, StdGen)
+runIteration :: (Int, Int) -> Int -> IntSet.IntSet -> StdGen -> (Int, StdGen)
 runIteration rng 0 xs gen = (0, gen)
 runIteration rng n xs gen =
   let (x, newGen) = randomR rng gen :: (Int, StdGen)
-  in if x `elem` xs then (1, newGen) else runIteration rng (n - 1) (x:xs) newGen
+  in if IntSet.member x xs then (1, newGen) else runIteration rng (n - 1) (IntSet.insert x xs) newGen
 
 main = do
   start <- getTimeMillis
   let iterations = 100000 :: Int
   gen <- getStdGen
-  -- let results = (/ (fromIntegral iterations)) . fromIntegral . length . filter id . simulate iterations $ gen
   let results = (/ (fromIntegral iterations)) . fromIntegral . simulate' iterations $ gen
   putStrLn . ("iterations: " ++) . show $ iterations
   putStrLn . ("sample-size: " ++) . show $ 23
