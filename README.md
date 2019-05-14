f89409483201508c5b2004dfb51b64f48aa8d165
patrickneugebauer <patrickneugebauer@users.noreply.github.com>
2019-05-13 21:55:42 -0400

    Add racket solution.

--- README.md
+++ README.md
@@ -1,36 +1,37 @@
 #### Birthday Paradox - Monte Carlo simulations
 
 * sample-size: 23
-* probability: 50.89
+* probability: 50.74
 
 | | language | iterations/sec |
 |--|--|--|
-1|c|4,032,258
-2|crystal|2,515,723
-3|vb.net|2,341,920
-4|csharp|2,328,288
-5|rust|2,143,622
-6|kotlin|1,986,097
-7|java|1,943,634
-8|dart|1,694,915
-9|go|1,342,281
-10|javascript|1,044,932
-11|julia|592,417
-12|php|454,132
-13|hack|241,312
-14|ruby|240,963
-15|python3|183,150
-16|python|157,232
+1|c|3,974,562
+2|crystal|2,528,445
+3|vb.net|2,355,712
+4|csharp|2,293,577
+5|rust|2,134,471
+6|java|1,980,198
+7|kotlin|1,910,219
+8|dart|1,702,127
+9|go|1,349,527
+10|javascript|1,050,420
+11|julia|607,533
+12|php|462,534
+13|hack|249,750
+14|ruby|240,384
+15|python3|186,915
+16|python|162,866
 17|perl|156,006
-18|fsharp|108,932
-19|haskell|108,695
-20|swift|98,231
-21|R|74,850
-22|elixir|20,790
-23|octave|9,066
-24|scheme|4,533
-25|perl6|4,156
-26|bash|3,881
-27|powershell|409
+18|racket|123,152
+19|haskell|109,890
+20|fsharp|109,170
+21|swift|101,729
+22|R|73,818
+23|elixir|22,598
+24|octave|9,041
+25|scheme|4,428
+26|perl6|4,212
+27|bash|3,894
+28|powershell|396
 
 thanks [Anthony Robinson](https://github.com/anthonycrobinson) for the tip about randint and random speed in python
---
+++ racket/loops.rkt
@@ -0,0 +1,69 @@
+#lang racket
+; ==================================================
+; functions
+; ==================================================
+
+(define (random-day) (random 365))
+
+(define (repeat num fn)
+  (let loop ((n num) (f fn) (acc '()))
+    (if (> n 0)
+      (loop (- n 1) f (cons (fn) acc))
+      acc)))
+
+(define (round-to n x)
+  (let ((shifter (expt 10 n)))
+    (/
+      (round (* x shifter))
+      shifter)))
+
+; ==================================================
+; main method code
+; ==================================================
+(define start (current-inexact-milliseconds))
+(define iterations 100000)
+(define sample-size 23)
+
+(define data
+  (repeat iterations
+    (lambda () (repeat sample-size random-day))))
+
+(define percent
+  (*
+    (/
+      (length (filter check-duplicates data))
+      iterations)
+    100))
+
+(define formatted-percent
+  (round-to 2
+    (exact->inexact percent)))
+
+(display
+  (string-append
+    "iterations: "
+    (number->string iterations)
+    "\n"))
+(display
+  (string-append
+    "sample-size: "
+    (number->string sample-size)
+    "\n"))
+(display
+  (string-append
+    "percent: "
+    (number->string formatted-percent)
+    "\n"))
+(define seconds
+  (exact->inexact
+    (/
+      (round
+        (-
+          (current-inexact-milliseconds)
+          start))
+      1000)))
+(display
+  (string-append
+    "seconds: "
+    (number->string seconds)
+    "\n"))
