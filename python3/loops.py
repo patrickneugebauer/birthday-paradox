e45b98d787d34aa72601ba492c2c2f823ab81ed7
patrickneugebauer <patrickneugebauer@users.noreply.github.com>
2020-07-25 12:01:18 -0400

    Update python3 solution with alternatives.

--- python3/loops.py
+++ python3/loops.py
@@ -1,6 +1,7 @@
 from random import random
 import time
 import sys
+# import array
 
 def simulate():
     start = time.time()
@@ -8,20 +9,29 @@ def simulate():
     sample_size = 23
 
     count = 0
-    for x in range(iterations):
-        data = [0] * 365
+    for _ in range(iterations):
+        # data = [0] * 365
+        data = set() # similar speed
+        # data = { -1 } # similar speed
+        # data = set([]) # similar speed
+        # data = [] # 33% slower
+        # data = [None for _ in range(365)] # 3x slower
+        # data = array.array('H', [0] * 365) # 3.5x slower
 
-        for i in range(sample_size):
+        for _ in range(sample_size):
             rand = int(random() * 365)
-            if data[rand] == 1:
-                count += 1;
+            # if data[rand] == 1: # for array and list[] solutions
+            if rand in data: # for set and x in list solutions
+                count += 1
                 break
             else:
-                data[rand] = 1;
+                # data[rand] = 1 # for array and list[] solutions
+                data.add(rand) # for set solution
+                # data.append(rand) # for x in list solution
 
     print("iterations:", iterations)
     print("sample-size:", sample_size)
-    results = round(count / iterations * 100, 2);
+    results = round(count / iterations * 100, 2)
     print("percent:", results)
     end = time.time()
     diff = round(end-start, 3)
