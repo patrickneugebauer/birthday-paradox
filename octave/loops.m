49912099338f710b55a34e37eb36c9dc15758b99
patrickneugebauer <patrickneugebauer@users.noreply.github.com>
2021-06-14 20:21:40 -0400

    Fix octave.

--- octave/loops.m
+++ octave/loops.m
@@ -1,5 +1,10 @@
 args = argv();
 iterations = str2num(args{1});
 script_dir = fileparts(mfilename('fullpath'));
-source(strcat(script_dir, "/", "iterate.m"));
+
+% no need to source this, octave will read it as a function file not a script file
+% https://octave.org/doc/v4.0.3/Function-Files.html#Function-Files
+% https://octave.org/doc/v4.0.3/Script-Files.html#Script-Files
+% source(strcat(script_dir, "/", "iterate.m"));
+
 iterate(iterations)
