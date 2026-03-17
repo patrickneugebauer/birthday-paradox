462fb72d05bb386d2ac7b0a46b441447ecb06c7a
patrickneugebauer <15330429+patrickneugebauer@users.noreply.github.com>
2026-03-16 22:37:31 -0400

    Add alternative runtimes for javascript.

---
+++ solutions/javascript/loops-tjs.js
@@ -0,0 +1,30 @@
+// [ 'tjs', 'run', 'loops-tjs.js', '100000' ]
+const iterations = tjs.args[3];
+
+function simulate() {
+  const start = new Date().getTime()
+  const sampleSize = 23
+
+  let count = 0
+  for (let i = 0; i < iterations; i++) {
+    const arr = new Array(365)
+    for (let j = 0; j < sampleSize; j++) {
+      const rand = Math.floor(Math.random() * 365)
+      if (arr[rand]) {
+        count++
+        break
+      } else {
+        arr[rand] = 1
+      }
+    }
+  }
+  console.log(`iterations: ${iterations}`)
+  console.log(`sample-size: ${sampleSize}`)
+  const results = (count / iterations * 100).toFixed(2)
+  console.log(`percent: ${results}`)
+  const end = new Date().getTime()
+  const diff = ((end - start) / 1000).toFixed(3)
+  console.log(`seconds: ${diff}`)
+}
+
+simulate()
