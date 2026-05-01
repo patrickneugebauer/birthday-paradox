d80c9b9185b2dd6f9ecab81626b99f8db3aa71de
patrickneugebauer <15330429+patrickneugebauer@users.noreply.github.com>
2026-04-30 22:21:03 -0400

    Add solution for javascript with multiple arrays.

---
+++ solutions/javascript/loops-arrays.js
@@ -0,0 +1,32 @@
+const iterations = process.argv[2];
+
+function simulate() {
+  // declarations
+  const start = process.hrtime.bigint();
+  const sampleSize = 23
+  let count = 0
+  // loop
+  for (let i = 0; i < iterations; i++) {
+    const arr = new Array(365)
+    for (let j = 0; j < sampleSize; j++) {
+      const rand = Math.floor(Math.random() * 365)
+      if (arr[rand] === 1) {
+        count++
+        break
+      } else {
+        arr[rand] = 1
+      }
+    }
+  }
+  // calcs
+  const results = (count / iterations * 100).toFixed(2)
+  const end = process.hrtime.bigint();
+  const diff = (Number(end - start) / 1_000_000_000).toFixed(6)
+  // log
+  console.log(`iterations: ${iterations}`)
+  console.log(`sample-size: ${sampleSize}`)
+  console.log(`percent: ${results}`)
+  console.log(`seconds: ${diff}`)
+}
+
+simulate()
